0b4ce7826e51a4774dd8b86102566b566620dd10
Matthew Geheran <matthew.p.geheran@erdc.dren.mil>
2023-09-09 17:49:09 -0500

    Updates rapid_input_csv.py to be consistent with PEP 8.

--- rapid_utils/rapid_input_csv.py
+++ rapid_utils/rapid_input_csv.py
@@ -1,8 +1,9 @@
 """Tools for writing various RAPID input CSV files."""
 
-import numpy as np
 from csv import writer as csv_writer
+import numpy as np
 import geopandas as gpd
+
 
 def write_connectivity_file(flowline_file,
                             flowline_id_field_name,
@@ -26,36 +27,41 @@ def write_connectivity_file(flowline_file,
         Path to output file to which the connectivity CSV will be written.
     """
 
-    flowline_gdf=gpd.read_file(flowline_file)
-    flowline_id_array=flowline_gdf[flowline_id_field_name].values
-    downstream_id_array=flowline_gdf[downstream_id_field_name].values
+    flowline_gdf = gpd.read_file(flowline_file)
+    flowline_id_array = flowline_gdf[flowline_id_field_name].values
+    downstream_id_array = flowline_gdf[downstream_id_field_name].values
 
-    list_write=[]
+    list_write = []
     max_count_upstream = 0
 
     for flowline_id in flowline_id_array:
-        #For each flowline, get a list of all upstream identifiers
-        list_upstream_id = flowline_id_array[downstream_id_array == flowline_id]
-        #Count the number of the upstream identifiers
+        # For each flowline, get a list of all upstream identifiers
+        list_upstream_id = flowline_id_array[
+            downstream_id_array == flowline_id]
+
+        # Count the number of the upstream identifiers
         count_upstream = len(list_upstream_id)
         if count_upstream > max_count_upstream:
             max_count_upstream = count_upstream
-        #For each flowline, record the downstream identifier
-        downstream_id = downstream_id_array[flowline_id_array == flowline_id][0]
-        #Compile the flowline_id, downstream_id, count_upstream, and
-        #list_upstream_id for writing
-        list_write.append(np.concatenate([np.array([flowline_id, downstream_id,
-                                                    count_upstream]),
-                                          list_upstream_id]).astype(int))
 
-    with open(out_csv_file, 'w') as csv_file:
+        # For each flowline, record the downstream identifier
+        downstream_id = downstream_id_array[
+            flowline_id_array == flowline_id][0]
+
+        # Compile the flowline_id, downstream_id, count_upstream, and
+        # list_upstream_id for writing
+        list_write.append(
+            np.concatenate([
+                np.array([flowline_id, downstream_id,
+                          count_upstream]), list_upstream_id]).astype(int))
+
+    with open(out_csv_file, 'w', encoding='utf-8') as csv_file:
         connectivity_writer = csv_writer(csv_file)
         for row_list in list_write:
-            out = np.concatenate([row_list,np.repeat(0,max_count_upstream -
-                                                       row_list[2])])
+            out = np.concatenate([row_list, np.repeat(0, max_count_upstream -
+                                                      row_list[2])])
             connectivity_writer.writerow(out.astype(int))
 
-    print('Max count upstream:',max_count_upstream)
 
 def write_riv_bas_id_file(flowline_file,
                           flowline_id_field_name,
@@ -76,10 +82,11 @@ def write_riv_bas_id_file(flowline_file,
         Path to output file to which the riv_bas_id_file will be written.
     """
 
-    flowline_gdf=gpd.read_file(flowline_file)
-    flowline_id_array=flowline_gdf[flowline_id_field_name].values
+    flowline_gdf = gpd.read_file(flowline_file)
+    flowline_id_array = flowline_gdf[flowline_id_field_name].values
 
-    np.savetxt(out_csv_file,flowline_id_array,fmt='%i')
+    np.savetxt(out_csv_file, flowline_id_array, fmt='%i')
+
 
 def write_kfac_file(flowline_file,
                     connectivity_file,
@@ -113,12 +120,12 @@ def write_kfac_file(flowline_file,
         Name of the field containing the identifier of each flowline in the
         flowline file.
     length_id_field_name : str
-        Name of the field containing the length of each flowline (in m or km) in
-        the flowline file.
+        Name of the field containing the length of each flowline (in m or km)
+        in the flowline file.
     slope_id_field_name : str
-        Name of the field containing the slope of each flowline in the flowline
-        file. The slope can be unitless (i.e., m/m or km/km) or represented as a
-        percentage (i.e., m/m * 100 or km/km * 100).
+        Name of the field containing the slope of each flowline in the
+        flowline file. The slope can be unitless (i.e., m/m or km/km) or
+        represented as a percentage (i.e., m/m * 100 or km/km * 100).
     formula_type : int
         The formula from Tavakoly et al. 2016 to be used.
     input_length_units : str, optional
@@ -129,62 +136,63 @@ def write_kfac_file(flowline_file,
         percentage and will be divided by 100. Default is False.
     """
 
-    flowline_gdf=gpd.read_file(flowline_file)
-    flowline_id_array=flowline_gdf[flowline_id_field_name].values
-    flowline_id_list=list(flowline_id_array)
+    flowline_gdf = gpd.read_file(flowline_file)
+    flowline_id_array = flowline_gdf[flowline_id_field_name].values
+    flowline_id_list = list(flowline_id_array)
 
     connect_rivid_array = np.genfromtxt(connectivity_file, delimiter=',',
                                         usecols=0, dtype=int)
 
-    #The kfac file must be ordered the same as the connectivity file
-    #Rewrite the length and slope arrays to ensure this requirement is met
-    length_array=flowline_gdf[length_field_name].values
-    slope_array=flowline_gdf[slope_field_name].values
+    # The kfac file must be ordered the same as the connectivity file
+    # Rewrite the length and slope arrays to ensure this requirement is met
+    length_array = flowline_gdf[length_field_name].values
+    slope_array = flowline_gdf[slope_field_name].values
 
-    sort_idx=[]
+    sort_idx = []
     for connect_rivid in connect_rivid_array:
         sort_idx.append(flowline_id_list.index(connect_rivid))
 
-    length_array=length_array[sort_idx]
-    slope_array=slope_array[sort_idx]
+    length_array = length_array[sort_idx]
+    slope_array = slope_array[sort_idx]
 
-    if input_length_units=='km':
+    if input_length_units == 'km':
         length_array *= 1000.
     if input_slope_percent:
         slope_array /= 100.
 
-    #For Kini1, divide each river length Li by the reference wave celerity C0 of
-    #1 km per hour (1000 m per 3600 s):
-    if formula_type==1:
-        kfac_array=length_array/(1000./3600.) #Length in m divided by (1000 m
-                                              #per 3600 s)
+    # For Kini1, divide each river length Li by the reference wave celerity
+    # C0 of 1 km per hour (1000 m per 3600 s):
+    if formula_type == 1:
+        # Length in m divided by (1000 m per 3600 s)
+        kfac_array = length_array / (1000. / 3600.)
 
-    #For Kini2:
-    if formula_type==2:
-        Kini1_array=length_array/(1000./3600.)
-        length_slope_array=length_array/(slope_array**0.5)
-        eta_array=np.mean(Kini1)/np.mean(length_slope_array)
-        kfac_array=eta_array*length_slope_array
+    # For Kini2:
+    if formula_type == 2:
+        k_ini_1_array = length_array / (1000. / 3600.)
+        length_slope_array = length_array / (slope_array**0.5)
+        eta_array = np.mean(k_ini_1_array) / np.mean(length_slope_array)
+        kfac_array = eta_array*length_slope_array
 
-    #For Kini3:
-    if formula_type==3:
-        Kini1_array=length_array/(1000./3600.)
-        length_slope_array=length_array/(slope_array**0.5)
+    # For Kini3:
+    if formula_type == 3:
+        k_ini_1_array = length_array / (1000. / 3600.)
+        length_slope_array = length_array/(slope_array**0.5)
         percentile_5 = np.percentile(length_slope_array, 5)
         percentile_95 = np.percentile(length_slope_array, 95)
         length_slope_array[length_slope_array < percentile_5] = percentile_5
         length_slope_array[length_slope_array > percentile_95] = percentile_95
-        eta_array=np.mean(Kini1_array)/np.mean(length_slope_array)
-        kfac_array=eta_array*length_slope_array
+        eta_array = np.mean(k_ini_1_array)/np.mean(length_slope_array)
+        kfac_array = eta_array*length_slope_array
 
-    np.savetxt(out_csv_file,kfac_array,fmt='%.1f')
+    np.savetxt(out_csv_file, kfac_array, fmt='%.1f')
+
 
 def write_constant_x_file(connectivity_file,
                           out_csv_file,
                           x_value):
 
     """
-    Write a constant Muskingum x file. The Muskingum x parameter will be the 
+    Write a constant Muskingum x file. The Muskingum x parameter will be the
     same for all river reaches).
 
     Parameters
