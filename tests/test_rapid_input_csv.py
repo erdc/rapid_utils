903a58725c1a514cfcc5e427c0afa30800e8a809
Matthew Geheran <matthew.p.geheran@erdc.dren.mil>
2023-09-09 16:35:31 -0500

    Explicitly provides expected output within tests in test_rapid_input_csv.py.

--- tests/test_rapid_input_csv.py
+++ tests/test_rapid_input_csv.py
@@ -4,50 +4,11 @@ Test module for rapid_utils/rapid_input_csv.py.
 import os
 from rapid_utils import rapid_input_csv
 import numpy as np
-from numpy.testing import assert_almost_equal
+from numpy.testing import assert_array_equal, assert_almost_equal
 
 TEST_DIR = os.path.relpath(os.path.dirname(__file__))
 DATA_DIR = os.path.join(TEST_DIR, 'data')
 OUTPUT_DIR = os.path.join(TEST_DIR, 'output')
-
-def read_csv_file(fname, skip_header=0):
-    """
-    Read a CSV file into a NumPy array.
-
-    Parameters
-    ----------
-    fname : str
-        Name of file.
-    skip_header : int, optional
-        Number of lines to skip at the beginning of the file.
-
-    Returns
-    -------
-    data : ndarray
-        Contents of CSV as an array.
-    """
-
-    data = np.genfromtxt(fname, delimiter=',', skip_header=skip_header)
-
-    return data
-
-def compare_csv_files(output_file, benchmark_file, skip_header=0):
-    """
-    Check if the contents of two weight-table files match.
-
-    Parameters
-    ----------
-    output_file : str
-        Name of file to compare.
-    benchmark_file : str
-        Name of file to compare.
-    skip_header : int, optional
-        Number of lines to skip at the beginning of each file.
-    """
-
-    output = read_csv_file(output_file, skip_header=skip_header)
-    benchmark = read_csv_file(benchmark_file, skip_header=skip_header)
-    assert_almost_equal(output, benchmark)
 
 def test_write_connectivity_file():
     """
@@ -55,22 +16,27 @@ def test_write_connectivity_file():
     with benchmark.
     """
 
-    flowline_file=os.path.join(DATA_DIR,'NHDPlusV2_sample',
-                               'NHDPlusV2_sample.shp')
+    flowline_file=os.path.join(DATA_DIR,'nhdplusv2_sample',
+                               'nhdplusv2_sample.shp')
     flowline_id_field_name='COMID'
     downstream_id_field_name='DWNCOMID'
     out_csv_file=os.path.join(OUTPUT_DIR,
-                              'NHDPlusV2_sample_rapid_connect_test.csv')
+                              'nhdplusv2_sample_rapid_connect_test.csv')
 
     rapid_input_csv.write_connectivity_file(flowline_file,
                                             flowline_id_field_name,
                                             downstream_id_field_name,
                                             out_csv_file)
 
-    benchmark_file=os.path.join(DATA_DIR,'NHDPlusV2_sample',
-                                'NHDPlusV2_sample_rapid_connect.csv')
+    connectivity = np.genfromtxt(out_csv_file, delimiter=',', dtype=int)
+    
+    expected = [[3820329, 0, 2, 3820227, 3820229],
+                [3820227, 3820329, 2, 3820161, 3820163],
+                [3820161, 3820227, 0, 0, 0],
+                [3820163, 3820227, 0, 0, 0],
+                [3820229, 3820329, 0, 0, 0]]
 
-    compare_csv_files(out_csv_file, benchmark_file, skip_header=0)
+    assert_array_equal(connectivity, expected)
 
 def test_write_riv_bas_id_file():
     """
@@ -78,32 +44,33 @@ def test_write_riv_bas_id_file():
     benchmark.
     """
 
-    flowline_file=os.path.join(DATA_DIR,'NHDPlusV2_sample',
-                               'NHDPlusV2_sample.shp')
+    flowline_file=os.path.join(DATA_DIR,'nhdplusv2_sample',
+                               'nhdplusv2_sample.shp')
     flowline_id_field_name='COMID'
     out_csv_file=os.path.join(OUTPUT_DIR,
-                              'NHDPlusV2_sample_riv_bas_id_test.csv')
+                              'nhdplusv2_sample_riv_bas_id_test.csv')
 
     rapid_input_csv.write_riv_bas_id_file(flowline_file,
                                           flowline_id_field_name,
                                           out_csv_file)
 
-    benchmark_file=os.path.join(DATA_DIR,'NHDPlusV2_sample',
-                                'NHDPlusV2_sample_riv_bas_id.csv')
+    riv_bas_id = np.genfromtxt(out_csv_file, delimiter=',', dtype=int)
 
-    compare_csv_files(out_csv_file, benchmark_file, skip_header=0)
+    expected = [3820329, 3820227, 3820161, 3820163, 3820229]
+
+    assert_array_equal(riv_bas_id, expected)
 
 def test_write_kfac_file():
     """
     Verify that write_kfac_file produces a kfac CSV consistent with benchmark.
     """
 
-    flowline_file=os.path.join(DATA_DIR,'NHDPlusV2_sample',
-                               'NHDPlusV2_sample.shp')
-    connectivity_file=os.path.join(DATA_DIR,'NHDPlusV2_sample',
-                                   'NHDPlusV2_sample_rapid_connect.csv')
+    flowline_file=os.path.join(DATA_DIR,'nhdplusv2_sample',
+                               'nhdplusv2_sample.shp')
+    connectivity_file=os.path.join(DATA_DIR,'nhdplusv2_sample',
+                                   'nhdplusv2_sample_rapid_connect.csv')
     out_csv_file=os.path.join(OUTPUT_DIR,
-                              'NHDPlusV2_sample_kfac_test.csv')
+                              'nhdplusv2_sample_kfac_test.csv')
     flowline_id_field_name='COMID'
     length_field_name='LENGTHKM'
     slope_field_name='SLOPE'
@@ -115,10 +82,11 @@ def test_write_kfac_file():
                                     length_field_name, slope_field_name,
                                     formula_type, input_length_units)
 
-    benchmark_file=os.path.join(DATA_DIR,'NHDPlusV2_sample',
-                                'NHDPlusV2_sample_kfac.csv')
+    kfac = np.genfromtxt(out_csv_file, delimiter=',')
 
-    compare_csv_files(out_csv_file, benchmark_file, skip_header=0)
+    expected = [7934.5, 4338.8, 6032.0, 8023.5, 4534.1]
+
+    assert_array_equal(kfac, expected)
 
 def test_write_constant_x_file():
     """
@@ -126,16 +94,17 @@ def test_write_constant_x_file():
     consistent with benchmark.
     """
 
-    connectivity_file=os.path.join(DATA_DIR,'NHDPlusV2_sample',
-                                   'NHDPlusV2_sample_rapid_connect.csv')
+    connectivity_file=os.path.join(DATA_DIR,'nhdplusv2_sample',
+                                   'nhdplusv2_sample_rapid_connect.csv')
     out_csv_file=os.path.join(OUTPUT_DIR,
-                              'NHDPlusV2_sample_x_test.csv')
+                              'nhdplusv2_sample_x_test.csv')
 
     rapid_input_csv.write_constant_x_file(connectivity_file,
                                           out_csv_file,
                                           0.3)
 
-    benchmark_file=os.path.join(DATA_DIR,'NHDPlusV2_sample',
-                                'NHDPlusV2_sample_x.csv')
+    x = np.genfromtxt(out_csv_file, delimiter=',')
 
-    compare_csv_files(out_csv_file, benchmark_file, skip_header=0)
+    expected = [0.3, 0.3, 0.3, 0.3, 0.3]
+
+    assert_array_equal(x, expected)
